d6903bfad5e0b2a96d2608fb4032fa17d70d7180
hecking_killa <chris.hermann9397@gmail.com>
2025-08-22 10:54:40 +0200

    Navbar adjustments

--- src/components/navbar-01/logo.tsx
+++ src/components/navbar-01/logo.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import LogoImage from '../../assets/LogoPepe.png';
+import SchriftImage from '../../assets/PepeSchrift.png';
 import { Link } from 'react-router'
 
 export const Logo: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => (
-  <Link to="/home">
-
+  <Link to="/home" className="flex items-center">
     <img
       src={LogoImage}
       alt="Pepe Logo"
-      width={62}
-      height={16}
+      width={50}
+      height={50}
       {...props}
+    />
+    <img
+      src={SchriftImage}
+      alt="Pepe Schriftzug"
+      width={85}
+      className="ml-2 -mt-1 h-auto hidden md:block"
     />
   </Link>
 );
